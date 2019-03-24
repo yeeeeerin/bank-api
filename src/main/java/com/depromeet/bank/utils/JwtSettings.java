5d4990fc2d8583798f5b8253dcf7e4ecbdc75256
yerin <hepari2154@naver.com>
2019-03-25 00:28:22 +0900

    Fix jwtSettings properties 분리

--- src/main/java/com/depromeet/bank/utils/JwtSettings.java
+++ src/main/java/com/depromeet/bank/utils/JwtSettings.java
@@ -2,17 +2,21 @@ package com.depromeet.bank.utils;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "jwt", ignoreInvalidFields = true)
+@PropertySource("classpath:jwtSetting.properties")
 public class JwtSettings {
 
+    @Value("${jwt.tokenIssuer}")
     private String tokenIssuer;
 
+    @Value("${jwt.tokenSigningKey}")
     private String tokenSigningKey;
 
 }
