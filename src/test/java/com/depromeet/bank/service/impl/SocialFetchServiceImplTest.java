10b503e7a8863731b309e65138d3e58da49feea6
yerin <hepari2154@naver.com>
2019-03-24 22:22:01 +0900

    Test Social 관련 데이터 가져오는지 테스트

---
+++ src/test/java/com/depromeet/bank/service/impl/SocialFetchServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.depromeet.bank.service.impl;
+
+import com.depromeet.bank.dto.TokenDto;
+import com.depromeet.bank.vo.SocialMemberVo;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SocialFetchServiceImplTest {
+
+    private SocialFetchServiceImpl service = new SocialFetchServiceImpl();
+    private TokenDto tokenDto;
+
+    @Before
+    public void setup() {
+        this.tokenDto = new TokenDto( "KeA8P6SgKrzQM2rs0XQ7oybYyJndSAR9Qxk7sgopyNoAAAFpr8kSpg");
+    }
+    @Test
+    public void service_social() {
+
+        SocialMemberVo socialMemberVo= service.getSocialUserInfo(this.tokenDto);
+        assertThat(socialMemberVo.getUserName(), is("이예린"));
+    }
+
+}
