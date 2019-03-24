ac33a100fc49124aaec6db0ba41e91feb99ce0b5
yerin <hepari2154@naver.com>
2019-03-24 23:25:48 +0900

    Add jwt생성자

---
+++ src/main/java/com/depromeet/bank/utils/JwtFactory.java
@@ -0,0 +1,34 @@
+package com.depromeet.bank.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.depromeet.bank.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtFactory {
+
+    @Autowired
+    JwtSettings jwtSettings;
+
+    /*
+     * 유저의 권한정보로 토큰을 만듬(claim에는 여러 정보가 올 수 있다.)
+     * */
+    public String generateToken(Member member) {
+        String token;
+
+        token = JWT.create()
+                .withIssuer(jwtSettings.getTokenIssuer())
+                .withClaim("EMAIL", member.getName())
+                .sign(Algorithm.HMAC256(jwtSettings.getTokenSigningKey()));
+
+        log.info("token -- " + token);
+
+        return token;
+
+    }
+
+}
