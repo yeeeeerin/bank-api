5f1f219582a1fcc2ece1b7b222e0b3b979a7fd9e
JaeyeonKIM <foodsksms@gmail.com>
2019-04-02 22:58:55 +0900

    피드백 1차수정.. 아직 다 수정하지 못했습니다.

--- src/main/java/com/depromeet/bank/adaptor/openapi/OpenApiAdaptorImpl.java
+++ src/main/java/com/depromeet/bank/adaptor/openapi/OpenApiAdaptorImpl.java
@@ -25,28 +25,6 @@ public class OpenApiAdaptorImpl implements OpenApiAdaptor {
         this.restTemplate = restTemplate;
     }
 
-//    @Override
-//    public Optional<String> getAirPollutionByStationName(String stationName) {
-//        URI url = UriComponentsBuilder.newInstance()
-//                .scheme("http")
-//                .host("openapi.airkorea.or.kr")
-//                .path("/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty")
-//                .queryParam("stationName", URLEncoder.encode(stationName, "UTF-8"))
-//                .queryParam("dataTerm", "daily")
-//                .queryParam("pageNo", 1)
-//                .queryParam("numOfRows", 1)
-//                .queryParam("serviceKey", serviceKey)
-//                .queryParam("ver", 1.3)
-//                .build(true)
-//                .toUri();
-//        try {
-//            String response = restTemplate.getForObject(url, String.class);
-//            return Optional.of(response);
-//        } catch (RestClientException ex) {
-//           log.error("Failed to get open-api request");
-//           return Optional.empty();
-//        }
-//    }
     @Override
     public Optional<String> getAirPollutionByStationName(String stationName) {
         try {
